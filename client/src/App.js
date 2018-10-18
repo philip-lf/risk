4e2cdcced72a237021a3db3a597f4426e3a1e94d
Philip Fahim <philip.louis49@gmail.com>
2018-10-17 21:45:24 -0700

    feat: add node express server

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './App.css';
+import accessToken from './secret.js';
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
+
+class App extends Component {
+  componentDidMount() {
+    mapboxgl.accessToken = accessToken;
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/philip-lf/cjndkpizz7wrs2sob7xbv61dc',
+        center: [-74.50, 40],
+        zoom: 1.5
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div id="map"/>
+      </div>
+    );
+  }
+}
+
+export default App;
