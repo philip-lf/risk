0cb4b13823aa69f3aebf5ee7c6df8b3cfbdc77a8
Philip Fahim <philip.louis49@gmail.com>
2018-10-18 17:24:53 -0700

    feat: chat box on port 5000

--- server.js
+++ server.js
@@ -1,18 +1,35 @@
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
-var io = require('socket.io').listen(server);
-var PORT = 8181;
+var io = require('socket.io')(server);
+var port = process.env.PORT || 5000;
 
-app.use('/css', express.static(__dirname + '/css'));
-app.use('/js', express.static(__dirname + '/js'));
+// app.use('/css', express.static(__dirname + '/css'));
+// app.use('/js', express.static(__dirname + '/js'));
 app.use('/assets', express.static(__dirname + '/assets'));
 
-app.get('/', (req,res) => {
-    console.log('file::: ', __dirname + '/public/index.html');
-    res.sendFile(__dirname + '/public/index.html');
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(PORT, function() {
-    console.log('Listening on '+ server.address().port);
+io.on('connection', function(socket){
+    console.log('a user connected');
+
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', function(){
+      console.log('user disconnected');
+    });
+});
+
+app.get('/api', (req,res) => {
+    console.log('website has been opened');
+    res.send({ express: 'Hello From Express' });
+});
+
+server.listen(port, () => {
+    console.log('Listening on port: ' + port);
 });
--- src/App.js
+++ src/App.js
@@ -3,8 +3,25 @@ import './App.css';
 import Map from './Map.jsx';
 
 class App extends Component {
+  state = {
+    response: ''
+  };
+
+  componentDidMount() {
+    this.callApi()
+    .then(res => this.setState({ response: res.express }))
+    .catch(err => console.log('there is an error'));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api');
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    return body;
+  };
 
   render() {
+    console.log('response: ', this.state.response);
     return (
       <div>
         <Map />
